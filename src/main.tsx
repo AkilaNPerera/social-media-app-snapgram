ecfc89617740fdf7defa9da00027f05c24330afe
Akila Navodani <akila_n@epic.lk>
2024-12-02 11:50:18 +0530

    Initialized project

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import App from './App';
+
+//starting point of a react app
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <App/> //Tells react on which element the components should be rendered in.
+);
