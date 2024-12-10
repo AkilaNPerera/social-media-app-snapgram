d77428fbd9b36b81cf1380f46f03205dc5eecb88
Akila Navodani <akila_n@epic.lk>
2024-12-10 17:27:26 +0530

    Set up folder structure

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+//To wrap both the sign in and sign up forms
+
+export default AuthLayout
